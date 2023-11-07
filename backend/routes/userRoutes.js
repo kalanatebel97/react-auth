9398a21b76e4d39a02a7f41e007d821393326a82
kalanatebel97 <kalanamtebel97@gmail.com>
2023-11-07 21:12:28 +0530

    apis

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,17 @@
+import express from "express";
+import {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUser,
+} from "../controllers/userController.js";
+import { protect } from "../middleware/authMiddleware.js";
+const router = express.Router();
+
+router.post("/auth", authUser);
+router.post("/register", registerUser);
+router.post("/logout", logoutUser);
+router.route("/profile").get(protect, getUserProfile).put(protect, updateUser);
+
+export default router;
